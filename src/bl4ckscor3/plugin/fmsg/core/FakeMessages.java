c15272c0fa8909fa17309cb1908fc1109da72d57
bl4ckscor3 <donalddani9612@gmx.de>
2015-04-04 12:26:14 +0200

    registering to bl4ckkitCore

--- src/bl4ckscor3/plugin/fmsg/core/FakeMessages.java
+++ src/bl4ckscor3/plugin/fmsg/core/FakeMessages.java
@@ -30,12 +30,14 @@ public class FakeMessages extends JavaPlugin
 		getServer().getPluginManager().registerEvents(new JoinLeaveListener(), this);
 		getServer().getPluginManager().registerEvents(new ChatListener(this), this);
 		Config.createConfig(this);
+		bl4ckkitCore.registerPlugin(this);
 		bl4ckkitCore.getMessageManager().sendEnabledMessage(this);
 	}
 
 	@Override
 	public void onDisable()
 	{
+		bl4ckkitCore.unregisterPlugin(this);
 		bl4ckkitCore.getMessageManager().sendDisabledMessage(this);
 	}
 
